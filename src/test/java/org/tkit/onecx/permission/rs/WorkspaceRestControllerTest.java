5c6e5bd50393c06196b597c04f61d11601eca26c
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-02-23 15:48:34 +0100

    feat: added full impl workspace details endpoint

--- src/test/java/org/tkit/onecx/permission/rs/WorkspaceRestControllerTest.java
+++ src/test/java/org/tkit/onecx/permission/rs/WorkspaceRestControllerTest.java
@@ -6,6 +6,8 @@ import static org.mockserver.model.HttpResponse.response;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.core.Response;
@@ -17,7 +19,10 @@ import org.mockserver.model.JsonBody;
 import org.tkit.onecx.permission.bff.rs.controllers.WorkspaceRestController;
 
 import gen.org.tkit.onecx.permission.bff.rs.internal.model.ProductDTO;
+import gen.org.tkit.onecx.permission.bff.rs.internal.model.WorkspaceDetailsDTO;
 import gen.org.tkit.onecx.permission.client.model.Product;
+import gen.org.tkit.onecx.permission.client.model.Workspace;
+import gen.org.tkit.onecx.product.store.client.model.*;
 import io.quarkiverse.mockserver.test.InjectMockServerClient;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
@@ -91,6 +96,100 @@ class WorkspaceRestControllerTest extends AbstractTest {
 
         Assertions.assertNotNull(output);
         Assertions.assertEquals(2, Arrays.stream(output).toList().size());
+    }
+
+    @Test
+    void getDetailsByWorkspaceNameTest() {
+
+        String workspaceName = "test-workspace";
+        Workspace workspace = new Workspace();
+        workspace.name("test-workspace").workspaceRoles(Set.of("role1", "role2"));
+
+        // create mock rest endpoint
+        mockServerClient
+                .when(request().withPath("/v1/workspaces/name/" + workspaceName).withMethod(HttpMethod.GET))
+                .withId(MOCKID)
+                .respond(httpRequest -> response().withStatusCode(Response.Status.OK.getStatusCode())
+                        .withBody(JsonBody.json(workspace)));
+
+        List<Product> productsOfWorkspace = new ArrayList<>();
+        Product product1 = new Product();
+        product1.productName("product1");
+        Product product2 = new Product();
+        product2.productName("product2");
+        productsOfWorkspace.add(product1);
+        productsOfWorkspace.add(product2);
+
+        // create mock rest endpoint
+        mockServerClient
+                .when(request().withPath("/v1/workspaces/" + workspaceName + "/products").withMethod(HttpMethod.GET))
+                .withId("MOCKID2")
+                .respond(httpRequest -> response().withStatusCode(Response.Status.OK.getStatusCode())
+                        .withBody(JsonBody.json(productsOfWorkspace)));
+
+        List<String> productNames = List.of("product1", "product2");
+        ProductItemLoadSearchCriteria criteria = new ProductItemLoadSearchCriteria();
+        criteria.setProductNames(productNames);
+
+        ProductsLoadResult result = new ProductsLoadResult();
+
+        ProductsAbstract productsAbstract1 = new ProductsAbstract();
+        productsAbstract1.setName("product1");
+        MicrofrontendAbstract mfe1 = new MicrofrontendAbstract();
+        mfe1.appId("mfe1").appName("mfe1");
+        MicroserviceAbstract ms1 = new MicroserviceAbstract();
+        ms1.appId("ms1").appName("ms1");
+        productsAbstract1.setMicrofrontends(List.of(mfe1));
+        productsAbstract1.setMicroservices(List.of(ms1));
+
+        ProductsAbstract productsAbstract2 = new ProductsAbstract();
+        productsAbstract2.setName("product2");
+        MicrofrontendAbstract mfe2 = new MicrofrontendAbstract();
+        mfe2.appId("mfe2").appName("mfe2");
+        MicroserviceAbstract ms2 = new MicroserviceAbstract();
+        ms2.appId("ms2").appName("ms2");
+        productsAbstract2.setMicrofrontends(List.of(mfe2));
+        productsAbstract2.setMicroservices(List.of(ms2));
+
+        result.setStream(List.of(productsAbstract1, productsAbstract2));
+        result.setTotalElements(2L);
+        result.setNumber(0);
+        result.setSize(2);
+        result.setTotalPages(1L);
+
+        // create mock rest endpoint
+        mockServerClient
+                .when(request().withPath("/v1/products/load").withMethod(HttpMethod.POST)
+                        .withBody(JsonBody.json(criteria)))
+                .withId("MOCKID3")
+                .respond(httpRequest -> response().withStatusCode(Response.Status.OK.getStatusCode())
+                        .withBody(JsonBody.json(result)));
+
+        var output = given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .pathParam("workspaceName", workspaceName)
+                .get("/{workspaceName}/details")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(WorkspaceDetailsDTO.class);
+
+        Assertions.assertNotNull(output);
+        Assertions.assertEquals(2, output.getProducts().size());
+        Assertions.assertTrue(output.getWorkspaceRoles().contains("role1"));
+        Assertions.assertTrue(output.getWorkspaceRoles().contains("role2"));
+        Assertions.assertEquals(1, output.getProducts().get(0).getMfe().size());
+        Assertions.assertEquals(1, output.getProducts().get(0).getMs().size());
+        Assertions.assertNotNull(output.getProducts().get(0).getMfe().get(0).getAppId());
+
+        Assertions.assertEquals(1, output.getProducts().get(1).getMfe().size());
+        Assertions.assertEquals(1, output.getProducts().get(1).getMs().size());
+        Assertions.assertNotNull(output.getProducts().get(1).getMs().get(0).getAppId());
+
+        mockServerClient.clear(MOCKID);
+        mockServerClient.clear("MOCKID2");
+        mockServerClient.clear("MOCKID3");
     }
 
 }
