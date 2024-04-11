18a6e83cfedeb2696de71035333a50558b471fd8
jsteenke <146953549+jsteenke@users.noreply.github.com>
2024-04-11 06:57:39 +0200

    feat: add CRUD to permissions (#21)
    * feat: add CRUD to permissions
    
    * fix: format

--- src/main/java/org/tkit/onecx/permission/bff/rs/controllers/PermissionRestController.java
+++ src/main/java/org/tkit/onecx/permission/bff/rs/controllers/PermissionRestController.java
@@ -3,18 +3,21 @@ package org.tkit.onecx.permission.bff.rs.controllers;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+import org.tkit.onecx.permission.bff.rs.mappers.ExceptionMapper;
 import org.tkit.onecx.permission.bff.rs.mappers.PermissionMapper;
 import org.tkit.quarkus.log.cdi.LogService;
 
 import gen.org.tkit.onecx.permission.bff.rs.internal.PermissionApiService;
-import gen.org.tkit.onecx.permission.bff.rs.internal.model.PermissionPageResultDTO;
-import gen.org.tkit.onecx.permission.bff.rs.internal.model.PermissionSearchCriteriaDTO;
+import gen.org.tkit.onecx.permission.bff.rs.internal.model.*;
 import gen.org.tkit.onecx.permission.client.api.PermissionInternalApi;
+import gen.org.tkit.onecx.permission.client.model.Permission;
 import gen.org.tkit.onecx.permission.client.model.PermissionPageResult;
 
 @ApplicationScoped
@@ -29,6 +32,32 @@ public class PermissionRestController implements PermissionApiService {
     @Inject
     PermissionMapper mapper;
 
+    @Inject
+    ExceptionMapper exceptionMapper;
+
+    @Override
+    public Response createPermission(CreatePermissionRequestDTO createPermissionRequestDTO) {
+        try (Response response = permissionClient.createPermission(mapper.map(createPermissionRequestDTO))) {
+            PermissionDTO permissionDTO = mapper.map(response.readEntity(Permission.class));
+            return Response.status(response.getStatus()).entity(permissionDTO).build();
+        }
+    }
+
+    @Override
+    public Response deletePermission(String id) {
+        try (Response response = permissionClient.deletePermission(id)) {
+            return Response.status(response.getStatus()).build();
+        }
+    }
+
+    @Override
+    public Response getPermission(String id) {
+        try (Response response = permissionClient.getPermission(id)) {
+            PermissionDTO permissionDTO = mapper.map(response.readEntity(Permission.class));
+            return Response.status(response.getStatus()).entity(permissionDTO).build();
+        }
+    }
+
     @Override
     public Response searchPermissions(PermissionSearchCriteriaDTO permissionSearchCriteriaDTO) {
         try (Response response = permissionClient.searchPermissions(mapper.map(permissionSearchCriteriaDTO))) {
@@ -37,8 +66,21 @@ public class PermissionRestController implements PermissionApiService {
         }
     }
 
+    @Override
+    public Response updatePermission(String id, UpdatePermissionRequestDTO updatePermissionRequestDTO) {
+        try (Response response = permissionClient.updatePermission(id, mapper.map(updatePermissionRequestDTO))) {
+            PermissionDTO permissionDTO = mapper.map(response.readEntity(Permission.class));
+            return Response.status(response.getStatus()).entity(permissionDTO).build();
+        }
+    }
+
     @ServerExceptionMapper
     public Response restException(WebApplicationException ex) {
         return Response.status(ex.getResponse().getStatus()).build();
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ProblemDetailResponseDTO> constraint(ConstraintViolationException ex) {
+        return exceptionMapper.constraint(ex);
     }
 }
--- src/main/java/org/tkit/onecx/permission/bff/rs/mappers/PermissionMapper.java
+++ src/main/java/org/tkit/onecx/permission/bff/rs/mappers/PermissionMapper.java
@@ -4,10 +4,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
 
-import gen.org.tkit.onecx.permission.bff.rs.internal.model.PermissionPageResultDTO;
-import gen.org.tkit.onecx.permission.bff.rs.internal.model.PermissionSearchCriteriaDTO;
-import gen.org.tkit.onecx.permission.client.model.PermissionPageResult;
-import gen.org.tkit.onecx.permission.client.model.PermissionSearchCriteria;
+import gen.org.tkit.onecx.permission.bff.rs.internal.model.*;
+import gen.org.tkit.onecx.permission.client.model.*;
 
 @Mapper(uses = { OffsetDateTimeMapper.class })
 public interface PermissionMapper {
@@ -15,5 +13,11 @@ public interface PermissionMapper {
 
     @Mapping(target = "removeStreamItem", ignore = true)
     PermissionPageResultDTO map(PermissionPageResult pageResult);
+
+    CreatePermissionRequest map(CreatePermissionRequestDTO createPermissionRequestDTO);
+
+    UpdatePermissionRequest map(UpdatePermissionRequestDTO updatePermissionRequestDTO);
+
+    PermissionDTO map(Permission permission);
 
 }
